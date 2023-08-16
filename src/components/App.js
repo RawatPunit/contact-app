0ec071e8116148595e98e51cc8ea4b01dfe5d3ee
Punit Rawat <rawatpunit928@gmail.com>
2023-08-16 11:56:16 +0000

    just need to add delete function

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import { uuid } from 'uuidv4';
 import './App.css';
 import Header from './Header';
-import Addcontact from './AddContact';
+import AddContact from './AddContact';
 import ContactList from './ContactList';
-function App() {
 
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";     // store the contacts in th local storage key 
+  const [contacts, setContacts] = useState([]);   //initially the contacts will be an empty array
+  //-------------removed as using REACT HOOKS for dynamic data-------------
   // rendering a list of contacts
-  const contacts  = [
-    {
-      id : '1',
-      "name": "Punit",
-      "email" : "rawatpunit829@gmail.com"
-    },
-    {
-      id : '2',
-      "name": "Anjali",
-      "email" : "rawatanjali829@gmail.com"
-    },
-    {
-      id : '1',
-      "name": "Putin",
-      "email" : "rawatputin829@gmail.com"
-    }
-  ];
+  // const contacts  = [
+  //   {
+  //     id : '1',
+  //     "name": "Punit",
+  //     "email" : "rawatpunit829@gmail.com"
+  //   },
+  //   {
+  //     id : '2',
+  //     "name": "Anjali",
+  //     "email" : "rawatanjali829@gmail.com"
+  //   },
+  //   {
+  //     id : '1',
+  //     "name": "Putin",
+  //     "email" : "rawatputin829@gmail.com"
+  //   }
+  // ]; 
+  //------------not req. as we are using useState for dynamic data filling and deletion------------------
+
+  //gettign the CONTACT from the AddContact component 
+  const addcontactHandler = (contact) =>{
+      setContacts([...contacts,{id: uuid(),...contacts} ])   //get all the data from the ...contact and put it into a new contact
+  }
+
+  const removeContactHandler =(id) =>{
+    const newContactList = contacts.filter((contact) =>{
+      return contact.id!==id;
+    });
+
+    setContacts(newContactList);
+  }
+
+
+  //while reloading the data gets wipe, so to update the token using useEff
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  },[contacts])
+
+  // now get the item from the updated LOCAL_STPRAGE_Key or the updated TOKEN
+  useEffect(()=>{
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    //only retirve new token when a new token is available
+    if(retriveContacts){
+      setContacts(retriveContacts);
+    }  
+  },[contacts])
+
   return (
     <div className='ui container'>
       <Header/>
-      <Addcontact/>
+      {/* to pass the data from the child to the parent that is from the contact and email array to the parent app ----------adding "addcontactHandler function" */}
+      <AddContact addcontactHandler ={addcontactHandler}/>
       {/* passing contacts as props and contacts as array */}
       {/* yaha se direct COntactList ko jayega data for rendering */}
-      <ContactList contacts={contacts}/>  
+      <ContactList contacts={contacts} getContactID={removeContactHandler}/>  
     </div>
   );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,16 +1,19 @@
 import React from "react";
+// import user from '../images/user.png';
 
 //adding props because we need to access contact.name and contact.email
-const CardContent = (props) =>{
+const ContactCard = (props) =>{
+    const {id, name,email} = props.contact;  //destructed so that we dont have to write contact.name/props.conctact.name again and again
     return(
         <div className="item">
+            {/* <img className="ui avatar image" src={user} alt="user"/> */}
             <div className="content">
-                <div className="header">{contact.name}</div>
-                <div >{contact.email}</div>
+                <div className="header">{name}</div>
+                <div>{email}</div>
             </div>
             <i className="trash alternate outline icon"
-            style={{color: "red", marginTop: '7px'}}></i>
-            
+            style={{color: "red", marginTop: '7px'}}>
+            </i>
         </div>
     )
 };
